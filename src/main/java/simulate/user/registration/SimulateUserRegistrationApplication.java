4d294f0882e3fb9111adbfc8bc02fc80ba8e8d6a
armelon1 <armelon1@outlook.com>
2023-09-20 09:17:46 -0300

    initial commit

---
+++ src/main/java/simulate/user/registration/SimulateUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package simulate.user.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulateUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulateUserRegistrationApplication.class, args);
+	}
+
+}
